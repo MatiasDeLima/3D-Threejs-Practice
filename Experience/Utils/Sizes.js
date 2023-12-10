d5751278a923ea483c84003334bfb6c2c5b74084
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2023-12-09 21:12:21 -0300

    feat(): Initialize project

---
+++ Experience/Utils/Sizes.js
@@ -0,0 +1,16 @@
+export default class Sizes {
+    constructor() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.aspect = this.width / this.height;
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+        // se a tela diminue o aumeta o modelo tambem
+        window.addEventListener("resize", () => {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.aspect = this.width / this.height;
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+        })
+    }
+}
